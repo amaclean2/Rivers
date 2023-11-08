04987638e853659596b61ef47178473d80a06927
Andrew Maclean <andrew.n.maclean@gmail.com>
2023-11-07 16:06:32 -0800

    added conversations to main router

--- Routing/index.js
+++ Routing/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./Users')
 const adventuresRouter = require('./Adventures')
 const picturesRouter = require('./Pictures')
 const servicesRouter = require('./Services')
+const conversationsRouter = require('./Messages')
 const { requestLogger } = require('../Config/loggerMiddleware')
 
 const router = Router()
@@ -14,5 +15,6 @@ router.use('/users', usersRouter)
 router.use('/adventures', adventuresRouter)
 router.use('/pictures', picturesRouter)
 router.use('/services', servicesRouter)
+router.use('/conversations', conversationsRouter)
 
 module.exports = router
